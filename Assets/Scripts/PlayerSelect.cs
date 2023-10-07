accce5d7ea82d24f81fea2c52a2f6fe041ae9b56
Jean-Christophe Tremblay <43895419+jean03114@users.noreply.github.com>
2023-10-07 17:33:42 -0400

    Player select

--- Assets/Scripts/PlayerSelect.cs
+++ Assets/Scripts/PlayerSelect.cs
@@ -5,12 +5,15 @@ using UnityEngine.InputSystem;
 
 public class PlayerSelect : MonoBehaviour
 {
+	[SerializeField]
+	public int maxPlayers = 4;
     private ControllerActions controllerActions;
 
 	private void Awake()
 	{
 		controllerActions = new ControllerActions();
 		controllerActions.Gameplay.Return.performed += OnReturn;
+		controllerActions.Gameplay.Select.performed += OnSelect;
 	}
 
 	public void ButtonBack()
@@ -18,9 +21,22 @@ public class PlayerSelect : MonoBehaviour
         SceneLoader.Instance.LoadLevel("MainMenu");
     }
 
+	public void OnReturn(InputAction.CallbackContext context)
+	{
+		ButtonBack();
+	}
+
+	public void OnSelect(InputAction.CallbackContext context)
+	{
+		Debug.Log("select");
+		/* if(PlayerManager.Instance.PlayerManagerData.TotalNbPlayer < maxPlayers)
+			PlayerManager.Instance.PlayerManagerData.TotalNbPlayer++; */
+	}
+
 	private void OnDestroy()
 	{
 		controllerActions.Gameplay.Return.performed -= OnReturn;
+		controllerActions.Gameplay.Select.performed -= OnSelect;
 	}
 
 	private void OnEnable()
@@ -32,9 +48,4 @@ public class PlayerSelect : MonoBehaviour
     {
         controllerActions.Disable();
     }
-
-	public void OnReturn(InputAction.CallbackContext context)
-	{
-		ButtonBack();
-	}
 }
