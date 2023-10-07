fdf0ee446a7ad882b9aa90be36681f1844aed8a6
Maxime26s <35577093+Maxime26s@users.noreply.github.com>
2023-10-06 22:18:08 -0400

    spinning rayzzzzzzzz

--- Assets/Scripts/SpinningBg/SpinningRays.cs
+++ Assets/Scripts/SpinningBg/SpinningRays.cs
@@ -1,26 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SpinningRays : MonoBehaviour
 {
+    public GameObject foreground;
+    public GameObject background;
     public List<GameObject> foregroundList;
     public List<GameObject> backgroundList;
+    public float foregroundSpeed = 1f;
+    public float backgroundSpeed = 0.75f;
+    public float foregroundAlpha = 0.25f;
+    public float backgroundAlpha = 0.125f;
 
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < foregroundList.Count; i++)
         {
-            foregroundList[i].GetComponent<SpriteRenderer>().color = HSVToRGB(90 * i, 1f, 1f, 0.25f);
-            backgroundList[i].GetComponent<SpriteRenderer>().color = HSVToRGB(90 * i, 1f, 1f, 0.125f);
+            foregroundList[i].GetComponent<SpriteRenderer>().color = HSVToRGB(90 * i, 1f, 1f, foregroundAlpha);
+            backgroundList[i].GetComponent<SpriteRenderer>().color = HSVToRGB(90 * (foregroundList.Count - i - 1), 1f, 1f, backgroundAlpha);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        foreground.transform.Rotate(0, 0, -foregroundSpeed);
+        background.transform.Rotate(0, 0, -backgroundSpeed);
 
+        for(int i = 0; i < foregroundList.Count; i++)
+        {
+            var fgSr = foregroundList[i].GetComponent<SpriteRenderer>();
+            fgSr.color = HSVToRGB(90 * i + (int)foreground.transform.rotation.eulerAngles.z, 1f, 1f, fgSr.color.a);
+
+            var bgSr = backgroundList[i].GetComponent<SpriteRenderer>();
+            bgSr.color = HSVToRGB(90 * (foregroundList.Count - i - 1) + (int)background.transform.rotation.eulerAngles.z, 1f, 1f, bgSr.color.a);
+
+        }
     }
 
     public static Color HSVToRGB(int h, float s, float v, float alpha = 1f)
@@ -35,5 +53,12 @@ public class SpinningRays : MonoBehaviour
         color.a = Mathf.Clamp01(alpha);
 
         return color;
+    }
+
+    public int GetHueFromRGB(Color color)
+    {
+        Color.RGBToHSV(color, out float h, out _, out _);
+
+        return (int)(h * 360f) % 360;
     }
 }
