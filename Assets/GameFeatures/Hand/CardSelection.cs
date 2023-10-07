7564fc4d2f5abfd9989893083e9bf0323d0de895
Maxime26s <35577093+Maxime26s@users.noreply.github.com>
2023-10-07 16:17:28 -0400

    FDSAFDSA

--- Assets/GameFeatures/Hand/CardSelection.cs
+++ Assets/GameFeatures/Hand/CardSelection.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CardSelection : MonoBehaviour
 {
     public BeatBar beatBar;
 
-    public List<Card> cards;
+    public List<GameObject> cards;
     public int currentIndex;
 
     // Start is called before the first frame update
     void Start()
     {
+        currentIndex = cards.Count / 2;
         beatBar.OnHitEvent += OnHit;
     }
 
@@ -20,19 +22,13 @@ public class CardSelection : MonoBehaviour
         beatBar.OnHitEvent -= OnHit;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnHit(object sender, HitEventArgs args)
     {
-        if(args.Context.action.name == "Select")
+        if (args.Context.action.name == "Select")
         {
             Use(args);
         }
-        else if(args.Context.action.name == "Move")
+        else if (args.Context.action.name == "Move")
         {
             Move(args);
         }
@@ -44,11 +40,12 @@ public class CardSelection : MonoBehaviour
             return;
 
         //cards[currentIndex].Unselect();
+        cards[currentIndex].SetActive(true); // TO CHANGE FOR: 1. PLAY CARD UNHOVER ANIMATION
 
-        if(args.Context.ReadValue<float>() > 0)
+        if (args.Context.ReadValue<float>() > 0)
         {
             currentIndex++;
-            if(currentIndex >= cards.Count)
+            if (currentIndex >= cards.Count)
             {
                 currentIndex = 0;
             }
@@ -56,17 +53,20 @@ public class CardSelection : MonoBehaviour
         else
         {
             currentIndex--;
-            if(currentIndex < 0)
+            if (currentIndex < 0)
             {
                 currentIndex = cards.Count - 1;
             }
         }
 
         //cards[currentIndex].Select();
+        cards[currentIndex].SetActive(false); // TO CHANGE FOR: 1. PLAY CARD HOVER ANIMATION
+
     }
 
     void Use(HitEventArgs args)
     {
         //cards[currentIndex].PlayCard(args.Result);
+        Destroy(cards[currentIndex]); // TO CHANGE FOR: 1. USE CARD 2. PLAY CARD ANIMATION 3. SWAP AT INDEX FOR EMPTY CARD
     }
 }
