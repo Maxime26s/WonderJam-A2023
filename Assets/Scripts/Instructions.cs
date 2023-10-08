7b34d60f3a4d91bf9ffb9e7a57719667b24f62f3
Jean-Christophe Tremblay <43895419+jean03114@users.noreply.github.com>
2023-10-08 01:05:24 -0400

    need to instruct instruction on how to instruction

---
+++ Assets/Scripts/Instructions.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Instructions : MonoBehaviour
+{
+	private ControllerActions controllerActions;
+
+	private void Awake()
+	{
+		controllerActions = new ControllerActions();
+		controllerActions.Gameplay.Return.performed += OnReturn;
+	}
+
+	public void ButtonBack()
+    {
+        SceneLoader.Instance.LoadLevel("MainMenu");
+    }
+
+	private void OnEnable()
+    {
+        controllerActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controllerActions.Disable();
+    }
+
+	public void OnReturn(InputAction.CallbackContext context)
+	{
+		ButtonBack();
+	}
+
+	private void OnDestroy()
+	{
+		controllerActions.Gameplay.Return.performed -= OnReturn;
+	}
+}
